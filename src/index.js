481d6c5806246b66e38d4e27890dc04c22b44d54
gtkatakura <gt.katakura@gmail.com>
2018-02-27 21:03:45 -0300

    feat(app): babel react parcel-bundler

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Learn Regex - Hello World!</h1>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));

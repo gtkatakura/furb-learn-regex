0eea1db7fc47f743f0f3927ac6be77a0d52eb3cb
gtkatakura <gt.katakura@gmail.com>
2018-04-11 22:53:54 -0300

    fix(link): colocado prop-types

--- client/components/Link.js
+++ client/components/Link.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link as Base } from 'react-router';
 
-const Link = ({ to, disabled, ...props }) => (
+const Link = ({ disabled, ...props }) => (
   disabled
     ? <span {...props} />
-    : <Base to={to} {...props} />
+    : <Base {...props} />
 );
+
+Link.propTypes = {
+  disabled: PropTypes.bool,
+};
+
+Link.defaultProps = {
+  disabled: false,
+};
 
 export default Link;

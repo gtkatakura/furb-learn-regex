a45d35a8311f2b50aeacdc47137d481edc00e6b8
gtkatakura <gt.katakura@gmail.com>
2018-05-22 23:04:14 -0300

    fix(solutions): considerando numeros como simbolo

--- client/validations/solutionIsValid.js
+++ client/validations/solutionIsValid.js
@@ -1,12 +1,13 @@
 import { toRegex } from 'regex';
-
-const getAllLetters = text => [...text.matchAll(/[a-z]/)].map(([letter]) => letter);
+import symbolParser from 'regex/symbols/parser';
 
 const solutionIsValid = currentStep => solution => {
-  const lettersFromSolution = getAllLetters(solution || '');
+  if (solution) {
+    const lettersFromSolution = symbolParser.parse(solution);
 
-  if (lettersFromSolution.some(letter => !currentStep.symbols.includes(letter))) {
-    return 'A expressão regular especificada usa símbolos que não pertencem ao alfabeto da linguagem';
+    if (lettersFromSolution.some(letter => !currentStep.symbols.includes(letter))) {
+      return 'A expressão regular especificada usa símbolos que não pertencem ao alfabeto da linguagem';
+    }
   }
 
   const allInvalid = currentStep.words.invalids.every(

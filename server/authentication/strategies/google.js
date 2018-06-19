fb1f2b1391ab88a62ff2e3bde5324023e961e182
gtkatakura <gt.katakura@gmail.com>
2018-06-18 22:42:46 -0300

    feat(authentication): salvando email nos providers ao criar conta pelo google

--- server/authentication/strategies/google.js
+++ server/authentication/strategies/google.js
@@ -5,6 +5,7 @@ const create = ({ config, users }) => {
     clientID: config.get('authentication.google.clientId'),
     clientSecret: config.get('authentication.google.clientSecret'),
     callbackURL: '/api/authentication/google/redirect',
+    profileFields: ['id', 'displayName', 'email'],
   };
 
   if (!passportConfig.clientID) {
@@ -12,7 +13,13 @@ const create = ({ config, users }) => {
   }
 
   return new OAuth2Strategy(passportConfig, async (request, accessToken, refreshToken, profile, done) => {
-    const user = await users.findOrCreateUser(profile.displayName, 'google', profile.id);
+    const user = await users.findOrCreateUser(
+      profile.displayName,
+      'google',
+      profile.id,
+      profile.emails[0].value,
+    );
+
     return done(null, user);
   });
 };

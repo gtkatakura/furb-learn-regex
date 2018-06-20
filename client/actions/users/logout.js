a6c4c62c2eb23cf2f4b7e9330f7c5caea4c6f88e
gtkatakura <gt.katakura@gmail.com>
2018-06-20 20:04:29 -0300

    fix(logout): limpando cookies de professor

--- client/actions/users/logout.js
+++ client/actions/users/logout.js
@@ -4,6 +4,7 @@ const logout = () => dispatch => {
   cookies.remove('X-JWT-Token');
   cookies.remove('X-User-Id');
   cookies.remove('X-User-Name');
+  cookies.remove('X-User-IsProfessor');
 
   dispatch({ type: 'USER_LOGOUT' });
 };

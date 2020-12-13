6fc114909545cbec4879c068b77c296ee726ad11
gtkatakura <gt.katakura@gmail.com>
2020-12-12 21:57:22 -0300

    feat: upgrade babel 6 to 7, add support for ts

--- webpack/common.js
+++ webpack/common.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     module: {
       rules: [
-        { test: /.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+        { test: /\.(js|tsx?)$/, loader: 'babel-loader', exclude: /node_modules/ },
         { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
         { test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000' },
         { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
@@ -46,6 +46,7 @@ module.exports = {
       ],
     },
     resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json'],
       modules: [
         path.resolve('./client'),
         path.resolve('./shared'),
